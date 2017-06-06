aa96f5edfbc878f4cc43468a1dabca2e3a510e6b
Jacob Graving <jgraving@gmail.com>
2017-06-06 14:20:06 +0200

    Add CameraCalibration class

--- pinpoint/calibration.py
+++ pinpoint/calibration.py
@@ -19,187 +19,215 @@ import cv2
 import pickle
 import glob
 
-def calibrate_camera_still(image_files,  grid_size = (9,6), imshow = True, delay = 500):
 
-	""" Returns calibration parameters from calibration images
+class CameraCalibration:
 
-		Parameters
-		----------
-		image_files : list of str
-			List of file paths as strings (e.g. glob.glob("/path/to/files/*.jpg").
-		grid_size : tuple of int
-			Size of calibration grid (internal corners)
-		imshow : bool, (default = True)
-			Show the calibration images
-		delay : int, >=1 (default = 500)
-			Delay in msecs between each image for imshow
-			
-		Returns
-		-------
-		params : dict
-			Parameters for undistorting images.
+	def __init__(self, grid_shape=(9,6)):
 
-	"""
+		""" Class for calculating calibration parameters from calibration images
 
-	# termination criteria
-	criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+			Parameters
+			----------
+			grid_shape : tuple of int
+				Shape of calibration grid (internal corners)
 
-	# prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
-	objp = np.zeros((grid_size[1]*grid_size[0],3), np.float32)
-	objp[:,:2] = np.mgrid[0:grid_size[0],0:grid_size[1]].T.reshape(-1,2)
+			Returns
+			-------
+			self : class
+				CameraCalibration class instance.
 
-	# Arrays to store object points and image points from all the images.
-	objpoints = [] # 3d point in real world space
-	imgpoints = [] # 2d points in image plane.
+		"""
 
-	for filename in image_files:
-		img = cv2.imread(filename)
+		self.grid_shape = grid_shape
 
-		if type(img) != type(None):
+	def calibrate(self, image_files, imshow = True, delay = 500):
 
-			gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+		""" Returns calibration parameters from calibration images
 
-			# Find the chess board corners
-			ret, corners = cv2.findChessboardCorners(gray, grid_size, None, flags = (cv2.CALIB_CB_ADAPTIVE_THRESH))
-			#ret, corners = cv2.findCirclesGrid(gray, grid_size, None, flags = (cv2.CALIB_CB_ASYMMETRIC_GRID))
-            
-            # If found, add object points, image points (after refining them)
-			if ret == True:
-				objpoints.append(objp)
-				corners2 = cv2.cornerSubPix(gray, corners, (11,11), (-1,-1), criteria)
-				imgpoints.append(corners2)
+			Parameters
+			----------
+			image_files : str
+				File path to images (e.g. "/path/to/files/*.jpg")
+			grid_shape : tuple of int
+				Size of calibration grid (internal corners)
+			imshow : bool, (default = True)
+				Show the calibration images
+			delay : int, >=1 (default = 500)
+				Delay in msecs between each image for imshow
+				
+			Returns
+			-------
+			params : dict
+				Parameters for undistorting images.
 
+		"""
+
+		image_files = sorted(glob.glob(image_files))
+
+		# termination criteria
+		criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 300, 0.001)
+
+		# prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
+		objp = np.zeros((self.grid_shape[1]*self.grid_shape[0],3), np.float32)
+		objp[:,:2] = np.mgrid[0:self.grid_shape[0],0:self.grid_shape[1]].T.reshape(-1,2)
+
+		# Arrays to store object points and image points from all the images.
+		objpoints = [] # 3d point in real world space
+		imgpoints = [] # 2d points in image plane.
+
+		for filename in image_files:
+			img = cv2.imread(filename)
+
+			if type(img) != type(None):
+
+				gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+				# Find the chess board corners
+				ret, corners = cv2.findChessboardCorners(gray, self.grid_shape, None, flags = (cv2.CALIB_CB_ADAPTIVE_THRESH))
+				#ret, corners = cv2.findCirclesGrid(gray, grid_shape, None, flags = (cv2.CALIB_CB_ASYMMETRIC_GRID))
+	            
+	            # If found, add object points, image points (after refining them)
+				if ret == True:
+					objpoints.append(objp)
+					corners2 = cv2.cornerSubPix(gray, corners, (11,11), (-1,-1), criteria)
+					imgpoints.append(corners2)
+
+					if imshow == True:
+						img = cv2.drawChessboardCorners(img, self.grid_shape, corners2, ret)
+
+				# Draw and display the corners
 				if imshow == True:
-					img = cv2.drawChessboardCorners(img, grid_size, corners2, ret)
+					cv2.imshow('img',img)
+					cv2.waitKey(delay)
+	                
+		if imshow == True:
+			cv2.destroyAllWindows()
+			for i in range(5):
+				cv2.waitKey(1) 
 
-			# Draw and display the corners
-			if imshow == True:
-				cv2.imshow('img',img)
-				cv2.waitKey(delay)
-                
-	if imshow == True:
-		cv2.destroyAllWindows()
-		for i in range(5):
-			cv2.waitKey(1) 
+		if len(objpoints) > 0 and len(imgpoints) > 0:
+			ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
+			params = {"ret" : ret, "mtx" : mtx, "dist" : dist, "rvecs" : rvecs, "tvecs" : tvecs}
+	        
+			total_error = 0
+			for i in xrange(len(objpoints)):
+				imgpoints2, _ = cv2.projectPoints(objpoints[i], rvecs[i], tvecs[i], mtx, dist)
+				error = cv2.norm(imgpoints[i], imgpoints2, cv2.NORM_L2)/len(imgpoints2)
+				total_error += error 
+			mean_error = total_error/len(objpoints)
 
-	if len(objpoints) > 0 and len(imgpoints) > 0:
-		ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
-		params = {"ret" : ret, "mtx" : mtx, "dist" : dist, "rvecs" : rvecs, "tvecs" : tvecs}
-        
-		total_error = 0
-		for i in xrange(len(objpoints)):
-			imgpoints2, _ = cv2.projectPoints(objpoints[i], rvecs[i], tvecs[i], mtx, dist)
-			error = cv2.norm(imgpoints[i],imgpoints2, cv2.NORM_L2)/len(imgpoints2)
-			total_error += error 
-		mean_error = total_error/len(objpoints)
+			print "Calibration successful! Mean error: ", mean_error
 
-		print "Calibration successful! Mean error: ", mean_error
+			self.params = params
+			self.mean_error = mean_error
+			self.ret = ret
+			self.mtx = mtx
+			self.dist = dist
+			self.rvecs = rvecs
+			self.tvecs = tvecs
 
-	else:
-		print "No calibration points found!"
-		params = None
+		else:
+			print "No calibration points found!"
+			self.params = None
 
-	return params
-
-
-def undistort_image(image, params, crop = True):
-
-	""" Returns undistorted image using calibration parameters.
-
-		Parameters
-		----------
-		image : numpy_array 
-			Image to be undistorted
-		params : dict
-			Calibration parameters from calibrate_camera()
-		crop : bool
-			Crop the image to the optimal region of interest
-			
-		Returns
-		-------
-		dst : numpy_array
-			Undistorted image.
-
-	"""
-	try:
-		ret = params["ret"]
-		mtx = params["mtx"]
-		dist = params["dist"]
-		rvecs = params["rvecs"]
-		tvecs = params["tvecs"]
-	except:
-		raise TypeError("params must be 'dict'")
-
-	img = image
-	h,  w = img.shape[:2]
-	newcameramtx, roi = cv2.getOptimalNewCameraMatrix(mtx, dist, (w,h), 1, (w,h))
-
-	# undistort
-	mapx, mapy = cv2.initUndistortRectifyMap(mtx, dist, None, newcameramtx, (w,h), 5)
-	dst = cv2.remap(img, mapx, mapy, cv2.INTER_LINEAR)
+		return self
 
 
-	# crop the image
-	if crop:
-		x,y,w,h = roi
-		dst = dst[y:y+h, x:x+w]
+	def undistort_image(self, image, crop = True):
 
-	return dst
+		""" Returns undistorted image using calibration parameters.
 
-def save_calib(filename, params): 
-	""" Saves calibration parameters as '.pkl' file.
+			Parameters
+			----------
+			image : numpy_array 
+				Image to be undistorted
+			params : dict
+				Calibration parameters
+			crop : bool
+				Crop the image to the optimal region of interest
+				
+			Returns
+			-------
+			dst : numpy_array
+				Undistorted image.
 
-		Parameters
-		----------
-		filename : str
-			Path to save file, must be '.pkl' extension
-		params : dict
-			Calibration parameters to save
+		"""
 
-		Returns
-		-------
-		saved : bool
-			Saved successfully.
-	"""
-	if type(params) != dict:
-			raise TypeError("params must be 'dict'")
+		img = image
+		h,  w = img.shape[:2]
+		newcameramtx, roi = cv2.getOptimalNewCameraMatrix(mtx, dist, (w,h), 1, (w,h))
 
-	output = open(filename, 'wb')
+		# undistort
+		mapx, mapy = cv2.initUndistortRectifyMap(mtx, dist, None, newcameramtx, (w,h), 5)
+		dst = cv2.remap(img, mapx, mapy, cv2.INTER_LINEAR)
 
-	try:
-		pickle.dump(params, output)
-	except:
-		raise IOError("filename must be '.pkl' extension")
+		# crop the image
+		if crop:
+			x,y,w,h = roi
+			dst = dst[y:y+h, x:x+w]
 
-	output.close()
+		return dst
 
-	saved = True
+	def save_calib(filename): 
+		""" Saves calibration parameters as '.pkl' file.
 
-	return saved
+			Parameters
+			----------
+			filename : str
+				Path to save file, must be '.pkl' extension
 
-def load_calib(filename):
-	""" Loads calibration parameters from '.pkl' file.
+			Returns
+			-------
+			saved : bool
+				Saved successfully.
+		"""
+		if type(self.params) != dict:
+				raise TypeError("params must be 'dict'")
 
-		Parameters
-		----------
-		filename : str8
-			Path to load file, must be '.pkl' extension
-			
-		Returns
-		-------
-		params : dict
-			Parameters for undistorting images.
+		output = open(filename, 'wb')
 
-	"""
-	# read python dict back from the file
-	
-	pkl_file = open(filename, 'rb')
+		try:
+			pickle.dump(self.params, output)
+		except:
+			raise IOError("filename must be '.pkl' extension")
 
-	try:
-		params = pickle.load(pkl_file)
-	except:
-		raise IOError("File must be '.pkl' extension")
+		output.close()
 
-	pkl_file.close()
+		
+		self.saved = True
 
-	return params
+		return self.saved
+
+	def load_calib(filename):
+		""" Loads calibration parameters from '.pkl' file.
+
+			Parameters
+			----------
+			filename : str
+				Path to load file, must be '.pkl' extension
+				
+			Returns
+			-------
+			params : dict
+				Parameters for undistorting images.
+
+		"""
+		# read python dict back from the file
+		
+		pkl_file = open(filename, 'rb')
+
+		try:
+			self.params = pickle.load(pkl_file)
+		except:
+			raise IOError("File must be '.pkl' extension")
+
+		pkl_file.close()
+
+		self.ret = self.params["ret"]
+		self.mtx = self.params["mtx"]
+		self.dist = self.params["dist"]
+		self.rvecs = self.params["rvecs"]
+		self.tvecs = self.params["tvecs"]
+
+		self.loaded = True
+		return self.loaded
