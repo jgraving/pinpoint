30c07cc7cb719c8a94ded533f78cbdefec676f60
Jacob Graving <jgraving@gmail.com>
2018-08-02 15:19:20 +0200

    Update copyright year

--- pinpoint/CameraCalibration.py
+++ pinpoint/CameraCalibration.py
@@ -1,5 +1,5 @@
 """
-Copyright 2015-2017 Jacob M. Graving <jgraving@gmail.com>
+Copyright 2015-2018 Jacob M. Graving <jgraving@gmail.com>
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
